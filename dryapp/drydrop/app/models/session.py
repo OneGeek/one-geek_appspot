37564bc61e20fb1c13044ec47156c3799dcc6eb3
Benjamin DeLillo <bpd9116@gmail.com>
2010-03-12 11:36:13 -0500

    deleted

--- dryapp/drydrop/app/models/session.py
+++
@@ -1,8 +0,0 @@
-# -*- mode: python; coding: utf-8 -*-
-import logging
-import google.appengine.ext.db as db
-from drydrop.app.core.model import Model
-
-class Session(db.Expando, Model):
-    
-    pass
